4974d8068926c84ef5d66e9890bf7ca965ee8ac5
qiuchunliu <qchunliu@163.com>
2019-07-27 08:18:19 +0800

    v11_with_exam

--- src/main/codes/utils/parseJson.scala
+++ src/main/codes/utils/parseJson.scala
@@ -1,7 +1,9 @@
 package utils
 
+import com.alibaba.fastjson.asm.Type
 import com.alibaba.fastjson.{JSON, JSONObject}
 import redis.clients.jedis.Jedis
+
 import scala.util.matching.Regex
 
 object parseJson {
@@ -14,41 +16,50 @@ object parseJson {
         |  "sex":"男",
         |  "age":18,
         |"grade":{"gname":"三年八班",
-        |         "gdesc":"初三年级八班"
+        |         "gdesc":"初三年级八班",
+        |         "grade":{"gname":"三年八班",
+        |         "gc":"初三年级八班"
+        |         }
         |         }
         | }
       """.stripMargin
 
 //    val s = "{'name':'桔子桑'}"
-//    val jobj: JSONObject = JSON.parseObject(s)
+    val jobj: JSONObject = JSON.parseObject(jsons)
+
+    val str: String = jobj.getObject("grade", classOf[JSONObject])
+      .getObject("grade", classOf[JSONObject]).getString("gc")
+    println(str)
+
+
 //    val jso: String = jobj.getString("grade")
 //    println(JSON.parseObject(jso).getString("gdesc"))
 
 
 
 
-//    val jedis = new Jedis("t21", 6379)
+////    val jedis = new Jedis("t21", 6379)
+////
+//    val s = "dertyfdghjrtyfd2fjtyjefdawetrf"
 //
-    val s = "dertyfdghjrtyfd2fjtyjefdawetrf"
-
-    val re = new Regex("d(.*?)f")
-    println(re.findAllIn(s).toArray.mkString(","))
-
-
-    val regex = new Regex(""""name":"(.*?)",""") // dertyf,dghjrtyf,d2f,dawetrf
-
-    println(
-      regex
-        .findAllIn(jsons)
-//        .group(1)
-        .toArray
-        .map(e => {
-      e.substring(8, e.indexOf("""","""))
-    })
-        .mkString(","))
-
-
-    println(regex.findAllIn(s).toArray.mkString(","))
+//    val re = new Regex("d(.*?)f")
+//    println(re.findAllIn(s).toArray.mkString(","))
+//
+//
+//    val regex = new Regex(""""name":"(.*?)",""") // dertyf,dghjrtyf,d2f,dawetrf
+//
+//    println(
+//      regex
+//        .findAllIn(jsons)
+////        .group(1)
+//        .toArray
+//        .map(e => {
+//      e.substring(8, e.indexOf("""","""))
+//    })
+//        .mkString(","))
+//
+//
+//    println(regex.findAllIn(s).toArray.mkString(","))
 
   }
 
